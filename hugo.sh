ea17ef3227664bdf5251398ae302c6bcfbe25b5e
Dunklas <dunklas@gmail.com>
2025-03-12 14:42:32 +0100

    Prevent new containers when running hugo script (#86)
    By using the --rm flag.
    
    In addition I've set a name for the container

--- hugo.sh
+++ hugo.sh
@@ -3,6 +3,8 @@
 HUGO_VERSION="0.145.0"
 
 docker run \
+        --name devies-blog \
+        --rm \
         -p 1313:1313 \
         -v ${PWD}:/src \
         hugomods/hugo:exts-"$HUGO_VERSION" "$@"
